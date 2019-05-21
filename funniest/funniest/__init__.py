77f9a843ae3c50e400db1a07eef9b4c29eed87b0
charles <charlesgomes@us.ibm.com>
2019-05-21 15:16:21 -0700

    Adding package funniest

---
+++ funniest/funniest/__init__.py
@@ -0,0 +1,2 @@
+def joke():
+    return (u'Hear about the new restaurant called karma? There’s no menu: You get what you deserve.')
---
+++ funniest/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniest',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/charles2588/funniest',
+      author='Charles Gomes',
+      author_email='charles2588@gmail.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
